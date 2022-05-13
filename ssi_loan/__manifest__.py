a9e383018eb96e4eeae0761f5bb35fbf7ad5df54
Andhitia Rama <andhitia.r@gmail.com>
2022-05-13 15:36:45 +0700

    [ssi_loan] 14.0.1.0.0

---
+++ ssi_loan/__manifest__.py
@@ -0,0 +1,45 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Loan",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_master_data_mixin",
+        "ssi_transaction_confirm_mixin",
+        "ssi_transaction_ready_mixin",
+        "ssi_transaction_open_mixin",
+        "ssi_transaction_done_mixin",
+        "ssi_transaction_cancel_mixin",
+        "account",
+        "base_automation",
+        "configuration_helper",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
+        "data/ir_sequence_data.xml",
+        "data/sequence_template_data.xml",
+        "data/policy_template_data.xml",
+        "data/approval_template_data.xml",
+        "data/ir_actions_server_data.xml",
+        "data/base_automation_data.xml",
+        "menu.xml",
+        "views/res_config_settings_views.xml",
+        "views/loan_type_views.xml",
+        "views/loan_mixin_views.xml",
+        "views/loan_out_views.xml",
+        "views/loan_in_views.xml",
+    ],
+    "demo": [
+        "demo/account_account_demo.xml",
+        "demo/account_journal_demo.xml",
+        "demo/loan_type_demo.xml",
+    ],
+}
---
+++ ssi_loan/models/__init__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    res_company,
+    res_config_settings,
+    loan_type,
+    loan_mixin,
+    loan_out,
+    loan_in,
+    loan_payment_schedule_mixin,
+    loan_payment_schedule_in,
+    loan_payment_schedule_out,
+)
