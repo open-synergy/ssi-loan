ae4e435f0021c09f22df8964639b546d3c92c30f
mikevhe18 <viriyananda.michael@gmail.com>
2023-08-21 08:51:11 +0700

    [ssi_loan] Publish to PyPi

--- ssi_loan/__manifest__.py
+++ ssi_loan/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Loan",
-    "version": "14.0.1.5.2",
+    "version": "14.0.1.5.3",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
     "license": "LGPL-3",
