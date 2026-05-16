21a7f6db1ec20f501200007031b037f830dfba77
Andhitia Rama <andhitia.r@gmail.com>
2026-05-16 08:48:19 +0700

    [UPD] ssi_loan
    * Add unit tests

---
+++ ssi_loan/tests/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2024 PT. Open Source Integra Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import test_loan_type
+from . import test_loan_out
+from . import test_loan_in
